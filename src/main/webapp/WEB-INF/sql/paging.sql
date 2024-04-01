db54108a5dd879d841f36f8df8fb3c29d6e7f862
pmj8298 <pmj8298@naver.com>
2024-04-01 12:39:10 +0900

    paging.sql 추가

---
+++ src/main/webapp/WEB-INF/sql/paging.sql
@@ -0,0 +1,48 @@
+--테이블에 샘플 데이터를 추가: 1000개
+페이징: 데이터베이스에서 필요한 데이터를 일정한 갯수만큼 조회하는 방법
+        대량의 데이터를 10개씩 가져온다
+oracle
+1.10g : rownum
+2.11g : ROW_NUMBER()
+3.12c : fetch 문법 - 가장 속도가 빠름
+
+DELETE FROM BOARD
+  WHERE BNO IN(998,996,902,899,898)
+
+DECLARE
+ DATA1 VARCHAR2(100):='HTML 게시물 ';
+BEGIN
+  FOR I IN 11..1000
+  LOOP
+INSERT INTO BOARD ( BNO, MENU_ID,TITLE,CONTENT, WRITER, REGDATE, HIT) 
+VALUES            ( I, 'MENU01',DATA1||I,'내용', 'sky', sysdate, 0);
+  END LOOP;
+  COMMIT;
+END;
+/
+
+SELECT * FROM BOARD ORDER BY BNO DESC;
+
+2. ROW_NUMBER() 함수 사용
+
+SET TIMING ON
+SELECT *
+ FROM
+   ( SELECT  ROW_NUMBER() OVER(ORDER BY BNO DESC) RN,
+            BNO, MENU_ID, TITLE, CONTENT, WRITER, REGDATE, HIT
+      FROM  BOARD) T
+     WHERE  T.RN BETWEEN 11 AND 20;
+     
+     
+3. OFFSET .. FETCH ..
+SET TIMING ON
+SELECT BNO, 
+       MENU_ID, 
+       TITLE, 
+       CONTENT, 
+       WRITER, 
+       TO_CHAR(REGDATE, 'YYYY-MM-DD') REGDATE, 
+       HIT
+  FROM BOARD
+  ORDER BY BNO DESC
+  OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;
